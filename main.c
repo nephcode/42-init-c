37241ba3f4b0b23fbf115842830b231df25925b2
Neph <nephcohen@gmail.com>
2025-06-07 04:16:04 +0200

    ex2

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 
 
 int main(int argc, char *argv[])
-
 {
-
     int age = 20;
-
     if (age >= 18)
-
     {
-
         printf ("Vous etes majeur !\n");
-
+    }
+    elseif (age > 4 )  
+    {
+        printf("nope pas possible")
+    }
+    else {
+        printf("bla bla bla");
     }
 
     return 0;
-
 }
