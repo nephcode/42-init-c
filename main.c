d66c9f30778aff36d696c56b27f9b788b5ce3eef
Neph <nephcohen@gmail.com>
2025-06-17 09:58:23 +0200

    maj C

--- main.c
+++ main.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[])
+int main()
 {
     int age = 20;
+
     if (age >= 18)
     {
         printf ("Vous etes majeur !\n");
     }
     elseif (age > 4 )  
     {
-        printf("nope pas possible")
+        printf("nope pas possible\n");
     }
-    else {
-        printf("bla bla bla");
+    else 
+    {
+        printf("bla bla bla\n");
     }
 
     return 0;
