afe7fd98019bf0124605c79eca4a30c1176d34bf
Neph <nephcohen@gmail.com>
2025-06-24 18:28:07 +0200

    code add pair

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Prototype de la fonction d'affichage
+void affiche(int *tableau, int tailleTableau);
 
-int main()
+int main(int argc, char *argv[])
+
 {
-    int age = 20;
-
-    if (age >= 18)
-    {
-        printf ("Vous etes majeur !\n");
-    }
-    elseif (age > 4 )  
-    {
-        printf("nope pas possible\n");
-    }
-    else 
-    {
-        printf("bla bla bla\n");
-    }
-
+    int tableau[4] = {10, 15, 3};
+    // On affiche le contenu du tableau
+    affiche(tableau, 4);
     return 0;
+}
+
+void affiche(int *tableau, int tailleTableau){
+    int i;
+    for (i = 0 ; i < tailleTableau ; i++){
+        printf("%d\n", tableau[i]);
+    }
 }
