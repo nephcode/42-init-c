ab3391cb536c30ff86f8e5c51bbd42cc568cae14
Neph <nephcohen@gmail.com>
2025-06-30 19:16:48 +0200

    C courses 42

---
+++ char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+
+{
+
+    char lettre = 'A';
+
+    scanf("%c", &lettre);
+    printf("%c\n", lettre);
+
+
+    return 0;
+
+}
